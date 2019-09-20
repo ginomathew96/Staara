bb0d4fd1f87fd0890374f72fc48bc4dc4259adfa
vishnu <vishnu@inens.com>
2019-09-20 16:01:40 +0530

    removed Schema

--- models/Artist.js
+++ models/Artist.js
@@ -1,12 +1,48 @@
 
 
-var ArtistSchema = require("../schemas/Artist")
 var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+var validate = require("validator")
 
-var newuser = mongoose.model("Users", ArtistSchema.userSchema)
-var Posts = mongoose.model("Posts", ArtistSchema.Posts)
+var Schema = mongoose.Schema;
+var user = new Schema({
+    uid: { type: String, required: true, index: { unique: true } }, firstname: { type: String, required: true },
+    lastname: { type: String },
+    gender: { type: String }, mobileno: { type: String, required: true }, DOB: { type: Date },
+    country: { type: String }, state: { type: String }, city: { type: String },
+    zipcode: { type: String }, email: { type: String, validate: [validate.isEmail, 'invalid email'] }, Schemtype: { type: String },
+    userPreference: Schema.Types.Mixed, userPortfolio: Schema.Types.Mixed, Uploads: Schema.Types.Mixed, usertype: { type: String, required: true },
+    requests: [{
+        CreatedDate: { type: Date, default: Date.now },
+        Post: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'posts'
+        }, status: String
+    }]
+
+})
+var Posts = new Schema({
+    name: { type: String, required: true },
+    description: String,
+    PostedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Users'
+    }, CreatedDate: { type: Date, default: Date.now }, Industry: Schema.Types.Mixed,
+    requests: [{
+        RequestedBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Users'
+        },
+        CreatedDate: { type: Date, default: Date.now }, status: String
+    }], active: Boolean
+})
+
+// module.exports = {
+//     userSchema: userSchema
+// };
+
+
+
 module.exports = {
-    newuser: newuser,
-    Posts: Posts
+    newuser: mongoose.model("Users", user),
+    Posts: mongoose.model("Posts", Posts)
 }
